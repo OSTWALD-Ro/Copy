2070c1c823736317c31f382d1d49174c779b0ee0
OSTWALD-Ro <139101764+OSTWALD-Ro@users.noreply.github.com>
2023-09-17 08:06:38 -0600

    Create head_index.js

--- head_index.js
+++ head_index.js
@@ -1,1 +1,14 @@
+'use strict';
+    
+// Constantes
 
+// Variables
+
+// Funciones
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  // Código Despues de Cargar la Pagina Completa
+
+  main__index();
+});
